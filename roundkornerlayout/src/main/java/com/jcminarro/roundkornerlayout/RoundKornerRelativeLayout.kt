c38cf924b89b92d9bcf7a19ae1feba327b2a1afa
Jc Miñarro <josecarlos.minarro@gmail.com>
2017-06-21 13:55:12 +0200

    Create RoundKornerRelativeLayout

---
+++ roundkornerlayout/src/main/java/com/jcminarro/roundkornerlayout/RoundKornerRelativeLayout.kt
@@ -0,0 +1,28 @@
+package com.jcminarro.roundkornerlayout
+
+import android.content.Context
+import android.graphics.Canvas
+import android.util.AttributeSet
+import android.widget.RelativeLayout
+
+class RoundKornerRelativeLayout
+    @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) :
+        RelativeLayout(context, attrs, defStyleAttr) {
+    private val canvasRounder: CanvasRounder
+
+    init {
+        val array = context.obtainStyledAttributes(attrs, R.styleable.RoundKornerRelativeLayout, 0, 0)
+        val cornerRadius = array.getDimension(R.styleable.RoundKornerRelativeLayout_corner_radius, 0f)
+        array.recycle()
+        canvasRounder = CanvasRounder(cornerRadius)
+    }
+
+    override fun onSizeChanged(currentWidth: Int, currentHeight: Int, oldWidth: Int, oldheight: Int) {
+        super.onSizeChanged(currentWidth, currentHeight, oldWidth, oldheight)
+        canvasRounder.updateSize(currentWidth, currentHeight)
+    }
+
+    override fun draw(canvas: Canvas) = canvasRounder.round(canvas) { super.draw(canvas) }
+
+    override fun dispatchDraw(canvas: Canvas) = canvasRounder.round(canvas) { super.dispatchDraw(canvas)}
+}
